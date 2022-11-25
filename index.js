daa07aa6221ae256d9ef88bcaf740361d493716e
Ståle Tomten <stale.tomten@finn.no>
2022-11-25 11:45:06 +0000

    listen to PORT env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const template = require('./template');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/',template);
 
